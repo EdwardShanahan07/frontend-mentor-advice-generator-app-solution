cbcccc371902f24f8e5a30d931e18b961b9b33fb
Edward Shanahan <edwardshanahan357@gmail.com>
2022-12-26 13:08:52 +0000

    Add generateDOMElements function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
 const getData = (url) =>
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => generateDOMElements(data));
+
+const generateDOMElements = (data) => {
+  const quoteNumberElement = document.createElement("p");
+  const quoteElement = document.createElement("p");
+  const divElement = document.createElement("div");
+
+  quoteNumberElement.classList.add("card__id");
+  quoteElement.classList.add("card__quote");
+
+  divElement.appendChild(quoteNumberElement);
+
+  quoteNumberElement.textContent = data.slip.id;
+  quoteElement.textContent = data.slip.adivce;
+
+  divElement.appendChild(quoteElement);
+
+  return divElement;
+};
 
 getData("https://api.adviceslip.com/advice");
